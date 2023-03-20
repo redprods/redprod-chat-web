e52ec6ecc65393bbe0f70be43dd9eee6f7e5788e
Crazygo <Lymix2016@mail.ru>
2023-03-20 22:14:10 +0300

    MenuProfile

--- src/components/MenuProfile/MenuCard.jsx
+++ src/components/MenuProfile/MenuCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './MenuProfile.scss'
+import styles from './MenuProfile.module.scss'
 
 export const MenuCard = ({name,icon}) => {
   return (
--- src/components/MenuProfile/MenuProfile.jsx
+++ src/components/MenuProfile/MenuProfile.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import styles from './MenuProfile.scss'
+import styles from './MenuProfile.module.scss'
 import Image from 'next/image'
 
 import avatar_profile from 'assets/avatar-profile.png'
 import {SlSettings} from 'react-icons/sl'
 import { MenuCard } from "./MenuCard";
-import {HiOutlineChatBubbleLeftRight} from 'react-icons/hi'
+import {BsChat} from 'react-icons/bs'
+import {HiOutlineUserPlus, HiOutlineUsers} from 'react-icons/hi2'
+import {IoExitOutline} from 'react-icons/io5'
 
 
 export const MenuProfile = () => {
   return <div className={styles.menuProfile}>  
-    <div className="Menu-Profile">
-      <div className="Menu-Logo">
+    <div className={styles.Menu_Profile}>
+      <div className={styles.Menu_Logo}>
           <Image src={avatar_profile} />
+          <div>
           <h2>Parviz Hello</h2>
           <p>@c10wn</p>
+          </div>
       </div>
       <div>
         <MenuCard name="Setting" icon={<SlSettings/>}/> 
-        <MenuCard name="Chat" icon={<HiOutlineChatBubbleLeftRight/>}/> 
-        <MenuCard name="Add Friend"/> 
-        <MenuCard name="Create Group"/>
-        <MenuCard name="Log Out"/> 
+        <MenuCard name="Chat" icon={<BsChat/>}/> 
+        <MenuCard name="Add Friend" icon={<HiOutlineUserPlus/>}/> 
+        <MenuCard name="Create Group" icon={<HiOutlineUsers/>}/>
+        <MenuCard name="Log Out" icon={<IoExitOutline/>}/> 
       </div>
     </div>
   
